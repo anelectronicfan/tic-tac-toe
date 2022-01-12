258de2a2f813902bea7785b6f812d21af3c214c5
Yu Fan Zheng <anelectronicfan@Yus-MacBook-Pro.local>
2022-01-12 16:21:17 +1100

    working tic tac toe with score counter

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 //Tic-tac-toe
 
 //DECLARE GLOBAL VARIABLES
-const cellArray = Array.from(document.querySelectorAll('.cell'));   //Array.from is a method that converts an array-like object (like a nodelist) into an array
+const cellArray = document.querySelectorAll('.cell');   //Array.from is a method that converts an array-like object (like a nodelist) into an array
 const playerTurnNode = document.querySelector('.playerTurn');
 let currentPlayer = 'X';
+let xScore = 0;
+let oScore = 0;
+let roundsPlayed = 1;
+const xScoreNode = document.querySelector('#xScore');
+const oScoreNode = document.querySelector('#oScore');
 
-let board = ['', '', '', '', '', '', '', ''];
+let board = ['', '', '', '', '', '', '', '', ''];
     //Board looks like this
     //[0][1][2]
     //[3][4][5]
@@ -25,17 +30,23 @@ const winningIndexes = [
 
 
 //DEFINE FUNCTIONS
+
 //Function that handles the flow of the game
 const playerClickEvents = function (cell, index) {
     if (!checkValid(cell)) {
         return;
     }
-    assignCellToPlayer(index);
+    assignCellToPlayer(cell, index);
     cell.innerText = currentPlayer;
-    checkWin();
+    gameEndEvents(checkWin());
     switchPlayer();
 }
-
+//Add event listeners to each cell
+cellArray.forEach( function (cell, index) {
+    cell.addEventListener('click', function () {
+        playerClickEvents(cell, index);
+    });
+});
 
 
 //Function that checks if a given cell can be assigned a value or not
@@ -45,24 +56,98 @@ const checkValid = function (cell) {
 }
 
 //Function that assigns a clicked cell to the player that clicked it
-const assignCellToPlayer = function (boardIndex) {
+const assignCellToPlayer = function (cell, boardIndex) {
+    cell.classList.remove(`player${currentPlayer}`);
     board[boardIndex] = currentPlayer;
+    cell.classList.add(`player${currentPlayer}`);
 }
 
 //Function that changes the player's turn
 const switchPlayer = function () {
-    currentPlayer = currentPlayer === 'X' ? '0': 'X';
+    currentPlayer = currentPlayer === 'X' ? 'O': 'X';
     playerTurnNode.id = playerTurnNode.id === 'playerX' ? 'playerO' : 'playerX';
     playerTurnNode.innerText = `Player ${currentPlayer}'s Turn.`;
 }
 
-//Function that checks if the game has been won or tied
+//Function that checks if the game has been 'win' or 'tied'
 const checkWin = function () {
+    let currentPlayerCellIndexes = [];
 
+    //loop through the board array, pushing the indexes of the currentPlayer's cells into currentPlayerCellIndexes
+    for (let i = 0; i < board.length; i ++) {
+        if (board[i] === currentPlayer) {
+            currentPlayerCellIndexes.push(i);
+        }
+    }
+    //loop through the winningIndexes array, checking if currentPlayerCellIndexes contains any of the winning combinations
+    for (let i = 0; i < winningIndexes.length; i++) {
+        let a = winningIndexes[i][0];
+        let b = winningIndexes[i][1];
+        let c = winningIndexes[i][2];
+
+        if (currentPlayerCellIndexes.includes(a) && currentPlayerCellIndexes.includes(b) && currentPlayerCellIndexes.includes(c)) {
+            if (currentPlayer === "X") {
+                xScore ++;
+                roundsPlayed++;
+                xScoreNode.innerText = `Player 1 Score: ${xScore}`;
+
+
+            } else {
+                oScore ++;
+                roundsPlayed++;
+                oScoreNode.innerText = `Player 2 Score: ${oScore}`;
+
+            }
+            return 'win';
+        }
+    }
+
+    if (!board.includes('')) {
+        return 'tie';
+    } else {
+        return false;   //reflects the state where game hasn't ended
+    }
 
 }
 
-//Add event listeners to each cell
-cellArray.forEach( function (cell, index) {
-    cell.addEventListener('click', playerClickEvents);
-})
+//Function that tells the game what to do when the game ends
+const gameEndEvents = function (checkWin) {
+    if (!checkWin) {
+        return; //If game hasn't ended, do nothing and return out of this function
+    } 
+    
+    if (checkWin === 'win') {
+        console.log(`Game Won By Player ${currentPlayer}!`);
+    } else if (checkWin === 'tie') {
+        console.log('The game has been tied!');
+    }
+    initialiseBoard();
+}
+
+//Function that resets the game board when a round is won or tied
+const initialiseBoard = function () {
+
+    cellArray.forEach( function (cell, index) {
+        cell.removeEventListener('click', function () {
+            playerClickEvents(cell, index);
+        });
+    });
+
+    setTimeout(function () {
+        board = ['', '', '', '', '', '', '', '', ''];
+    cellArray.forEach( function (cell) {
+        cell.innerText = '';
+        cell.classList.remove('playerX');
+        cell.classList.remove('playerO');
+    })
+
+    cellArray.forEach( function (cell, index) {
+        cell.addEventListener('click', function () {
+            playerClickEvents(cell, index);
+        });
+    });
+    }, 1000)
+    
+}
+
+
